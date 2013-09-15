af5aa7ef24d57c09821f7179feab66f96c3e36d3
AtroCty <atrocty@gmx.de>
2013-09-15 23:51:07 +0200

    Finaly Test

--- src/io/github/atrocty/atrogpfix/atrogpfix.java
+++ src/io/github/atrocty/atrogpfix/atrogpfix.java
@@ -1,8 +1,16 @@
 package io.github.atrocty.atrogpfix;
 
-import me.ryanhamshire.GriefPrevention.Claim;
+import org.bukkit.Location;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public final class atrogpfix extends JavaPlugin 
+import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+
+public class atrogpfix extends JavaPlugin implements Cancellable
 {
 	@Override
 	public void onEnable()
@@ -16,24 +24,41 @@ public final class atrogpfix extends JavaPlugin
 		getLogger().info("onDisable wurde ausgeführt!");
 	}
 	
+	public static Claim getClaimAtLocation(Location loc)
+	{
+		return GriefPrevention.instance.dataStore.getClaimAt(loc, true, null);
+	}
+	
+	private boolean cancel = false;
+	
 	public class CreatureSpawnListener implements Listener
 	{
 		@EventHandler(ignoreCancelled = true)
+		
 		private void onCreatureSpawn(CreatureSpawnEvent e)
 		{
-			Claim claim = ClaimManager.getClaimAtLocation(e.getLocation());
-			World world = e.getEntity().getWorld();
-			Flag flag = new Flag(Type.SpawnMob);
-	
+			Claim claim = getClaimAtLocation(e.getLocation());
+			
 			if (e.getSpawnReason() == CreatureSpawnEvent.SpawnReason.NATURAL) 
 			{
-				if (claim == null) {
-				e.setCancelled(!flag.isUnclaimedAllowed(world));
-			} 
-			else 
-			{
-				e.setCancelled(!flag.isAllowed(claim));
+				if (claim != null) 
+				{
+					e.setCancelled(true);
+				}
 			}
 			return;
 		}
+	}
+
+	@Override
+	public boolean isCancelled() 
+	{
+		return cancel;
+	}
+
+	@Override
+	public void setCancelled(boolean cancel) 
+	{
+		this.cancel = cancel;
+	}
 }
